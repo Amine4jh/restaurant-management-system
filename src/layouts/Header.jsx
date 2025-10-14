b0f4991976a385c3734f47869cbc6deee6376d0e
Amine4jh <amine4jh@gmail.com>
2025-10-14 16:56:45 +0100

    Add tailwind theme, and fix header colors

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,30 +1,30 @@
 const Header = () => {
   return (
-    <header className="flex justify-between items-center py-8 px-15 bg-zinc-900 text-zinc-50">
-      <a href="" className="text-3xl font-bold">
-        RedSky
+    <header className="flex justify-between items-center py-8 px-15 bg-dark text-beige">
+      <a href="" className="text-primary text-3xl font-bold">
+        Red<span className="text-gold">Sky</span>
       </a>
       <div className="flex gap-10">
         <a
-          className="border-b-2 font-semibold border-transparent hover:border-zinc-400 py-1 hover:text-zinc-400 transition duration-300 ease hover:scale-110"
+          className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
           href=""
         >
           Restaurant
         </a>
         <a
-          className="border-b-2 font-semibold border-transparent hover:border-zinc-400 py-1 hover:text-zinc-400 transition duration-300 ease hover:scale-110"
+          className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
           href=""
         >
           Features
         </a>
         <a
-          className="border-b-2 font-semibold border-transparent hover:border-zinc-400 py-1 hover:text-zinc-400 transition duration-300 ease hover:scale-110"
+          className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
           href=""
         >
           About
         </a>
         <a
-          className="border-b-2 font-semibold border-transparent hover:border-zinc-400 py-1 hover:text-zinc-400 transition duration-300 ease hover:scale-110"
+          className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
           href=""
         >
           Contact
@@ -33,13 +33,13 @@ const Header = () => {
       <div className="flex items-center gap-8">
         <a
           href=""
-          className="p-2 border-2 border-transparent rounded font-bold hover:border-zinc-50 transition duration-300 ease hover:scale-110"
+          className="p-2 border-2 border-transparent rounded font-bold hover:border-beige transition duration-300 ease hover:scale-110"
         >
           Log In
         </a>
         <a
           href=""
-          className="p-2 bg-zinc-50 text-zinc-900 border-2 border-zinc-50 rounded font-bold hover:bg-transparent hover:text-zinc-50 transition duration-300 ease hover:scale-110"
+          className="p-2 bg-beige text-dark border-2 border-beige rounded font-bold hover:bg-transparent hover:text-beige transition duration-300 ease hover:scale-110"
         >
           Sign Up
         </a>
