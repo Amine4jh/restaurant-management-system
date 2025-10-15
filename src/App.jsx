f1831cf94613a38b5ce26bf4a123edbffcaeb3e2
Amine4jh <amine4jh@gmail.com>
2025-10-15 22:49:18 +0100

    Add routing using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import Header from "./layouts/Header";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/homePage";
+import Login from "./pages/Login";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <HomePage />
-    </>
-  );
+  const Routing = createBrowserRouter([
+    { path: "/", element: <HomePage /> },
+    { path: "*", element: <HomePage /> },
+    { path: "login", element: <Login /> },
+  ]);
+  return <RouterProvider router={Routing}></RouterProvider>;
 }
 
 export default App;
