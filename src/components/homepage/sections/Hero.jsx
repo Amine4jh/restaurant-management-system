906bc4d34863882ea15a5d0cbd90ca59230e42e7
Amine4jh <amine4jh@gmail.com>
2025-10-17 15:45:00 +0100

    Make Hero Section responsive

--- src/components/homepage/sections/Hero.jsx
+++ src/components/homepage/sections/Hero.jsx
@@ -2,26 +2,28 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="bg-[url(./images/home-bg.webp)] bg-no-repeat bg-cover">
-      <div className="backdrop-brightness-30 px-100 py-65 text-center">
-        <h1 className="text-beige text-6xl font-bold">
-          Experience the Taste of Perfection
-        </h1>
-        <h3 className="text-lightRed text-6xl font-bold">
-          Anytime, <span className="text-gold">Anywhere</span>
-        </h3>
-        <p className="text-beige px-5 my-10 text-xl opacity-75">
-          Welcome to <b>RedSky</b>, your all-in-one restaurant management and
-          dining experience. From table reservations to online orders — we've
-          got you covered.
-        </p>
-        <div className="flex items-center justify-center gap-5">
-          <Link
-            to={"/signup"}
-            className="text-xl px-5 py-2 bg-gold text-dark border-2 border-gold rounded font-bold hover:bg-transparent hover:text-gold transition duration-300 ease hover:scale-110"
-          >
-            Get Started For Free!
-          </Link>
+    <div className="bg-[url(./images/home-bg.webp)] bg-no-repeat bg-cover md:bg-top bg-center">
+      <div className="backdrop-brightness-30 py-65 text-center">
+        <div className="max-w-[1240px] lg:px-80 md:px-40 px-15 mx-auto">
+          <h1 className="text-beige lg:text-6xl text-4xl font-bold">
+            Experience the Taste of Perfection
+          </h1>
+          <h3 className="text-lightRed lg:text-6xl text-4xl font-bold">
+            Anytime, <span className="text-gold">Anywhere</span>
+          </h3>
+          <p className="text-beige px-5 my-10 md:text-xl text-lg opacity-75">
+            Welcome to <b>RedSky</b>, your all-in-one restaurant management and
+            dining experience. From table reservations to online orders — we've
+            got you covered.
+          </p>
+          <div className="flex items-center justify-center gap-5">
+            <Link
+              to={"/signup"}
+              className="lg:text-xl px-5 py-2 bg-gold text-dark border-2 border-gold rounded font-bold hover:bg-transparent hover:text-gold transition duration-300 ease hover:scale-110"
+            >
+              Get Started For Free!
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -10,87 +10,89 @@ const Header = () => {
   };
 
   return (
-    <header className="flex justify-between items-center py-8 px-15 bg-dark text-beige">
-      <div className="flex items-center gap-5">
-        {/* NavBar Icon (Menu) */}
-        <div onClick={handleNav} className="block lg:hidden">
-          <AiOutlineMenu size={25} />
-        </div>
+    <header className="bg-dark">
+      <div className="flex justify-between items-center max-w-[1240px] mx-auto py-8 px-10 bg-dark text-beige">
+        <div className="flex items-center gap-5">
+          {/* NavBar Icon (Menu) */}
+          <div onClick={handleNav} className="block lg:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
 
-        <Link to={"/"} className="text-primary text-3xl font-bold">
-          Red<span className="text-gold">Sky</span>
-        </Link>
-      </div>
-      <div className="hidden gap-10 lg:flex">
-        <Link
-          className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
-          to={"/"}
-        >
-          Restaurant
-        </Link>
-        <Link
-          className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
-          to={"/"}
-        >
-          Features
-        </Link>
-        <Link
-          className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
-          to={"/"}
-        >
-          About
-        </Link>
-        <Link
-          className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
-          to={"/"}
-        >
-          Contact
-        </Link>
-      </div>
-      <div className="flex items-center gap-8">
-        <Link
-          to={"/login"}
-          className="p-2 border-2 border-transparent rounded font-bold hover:border-beige transition duration-300 ease hover:scale-110"
-        >
-          Log In
-        </Link>
-        <Link
-          to={"/signup"}
-          className="p-2 bg-beige text-dark border-2 border-beige rounded font-bold hover:bg-transparent hover:text-beige transition duration-300 ease hover:scale-110"
-        >
-          Sign Up
-        </Link>
-      </div>
-
-      {/* Mobile NavBar */}
-      <div
-        className={
-          !nav
-            ? "fixed top-0 start-0 w-[60%] h-full border-r p-4 bg-dark ease-in-out duration-500 z-1"
-            : "fixed left-[-100%]"
-        }
-      >
-        <div className="flex items-center justify-between">
           <Link to={"/"} className="text-primary text-3xl font-bold">
             Red<span className="text-gold">Sky</span>
           </Link>
-          <div onClick={handleNav} className="block lg:hidden">
-            <AiOutlineClose size={20} />
-          </div>
         </div>
-        <div className="py-4">
-          <Link className="border-b font-semibold py-6 px-2 block" to={"/"}>
+        <div className="hidden gap-10 lg:flex">
+          <Link
+            className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
+            to={"/"}
+          >
             Restaurant
           </Link>
-          <Link className="border-b font-semibold py-6 px-2 block" to={"/"}>
+          <Link
+            className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
+            to={"/"}
+          >
             Features
           </Link>
-          <Link className="border-b font-semibold py-6 px-2 block" to={"/"}>
+          <Link
+            className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
+            to={"/"}
+          >
             About
           </Link>
-          <Link className="font-semibold py-6 px-2 block" to={"/"}>
+          <Link
+            className="border-b-2 font-semibold border-transparent hover:border-lightRed py-2 hover:text-lightRed transition duration-300 ease hover:scale-110"
+            to={"/"}
+          >
             Contact
           </Link>
+        </div>
+        <div className="flex items-center gap-8">
+          <Link
+            to={"/login"}
+            className="p-2 border-2 border-transparent rounded font-bold hover:border-beige transition duration-300 ease hover:scale-110"
+          >
+            Log In
+          </Link>
+          <Link
+            to={"/signup"}
+            className="p-2 bg-beige text-dark border-2 border-beige rounded font-bold hover:bg-transparent hover:text-beige transition duration-300 ease hover:scale-110"
+          >
+            Sign Up
+          </Link>
+        </div>
+
+        {/* Mobile NavBar */}
+        <div
+          className={
+            !nav
+              ? "fixed top-0 start-0 w-[60%] h-full border-r p-4 bg-dark ease-in-out duration-500 z-1"
+              : "fixed left-[-100%]"
+          }
+        >
+          <div className="flex items-center justify-between">
+            <Link to={"/"} className="text-primary text-3xl font-bold">
+              Red<span className="text-gold">Sky</span>
+            </Link>
+            <div onClick={handleNav} className="block lg:hidden">
+              <AiOutlineClose size={20} />
+            </div>
+          </div>
+          <div className="py-4">
+            <Link className="border-b font-semibold py-6 px-2 block" to={"/"}>
+              Restaurant
+            </Link>
+            <Link className="border-b font-semibold py-6 px-2 block" to={"/"}>
+              Features
+            </Link>
+            <Link className="border-b font-semibold py-6 px-2 block" to={"/"}>
+              About
+            </Link>
+            <Link className="font-semibold py-6 px-2 block" to={"/"}>
+              Contact
+            </Link>
+          </div>
         </div>
       </div>
     </header>
